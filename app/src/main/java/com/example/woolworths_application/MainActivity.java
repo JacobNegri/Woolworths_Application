91ce153303cf95002a5c8cdd7c339554ad9daf68
jc319816 <jnegr3@gmail.com>
2019-11-12 15:01:56 +1000

    re-commit full project as previous additions prevented it from working.

---
+++ app/src/main/java/com/example/woolworths_application/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.woolworths_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int counter = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name = (EditText)findViewById(R.id.LoginName);
+        Password = (EditText)findViewById(R.id.LoginPassword);
+        Info = (TextView)findViewById(R.id.Info);
+        Login = (Button)findViewById(R.id.btnLogin);
+
+        Info.setText("Number of Attempts remaining: 5");
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(),Password.getText().toString());
+            }
+        });
+    }
+
+    private void validate(String userName, String userPassword){
+        if ((userName.equals("Admin")) && (userPassword.equals("1234"))){
+            Intent intent = new Intent(MainActivity.this, MainPage.class);
+            startActivity(intent);
+        }else{
+            counter--;
+
+            Info.setText("Number of Attempts remaining: " + String.valueOf(counter));
+
+            if (counter == 0){
+                Login.setEnabled(false);
+            }
+        }
+    }
+}
+
